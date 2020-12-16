e3f5d741086bdbced4e273c49ceb512b39922483
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-16 16:36:57 +0530

    [Pratibha Mastud] Add: Create Stack Data Structure

---
+++ StackDataStruct/StackDataStruct/LinkListStack.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackDataStruct
+{
+    class LinkListStack
+    {
+        private Node top;
+        public LinkListStack()
+        {
+            this.top = null;
+        }
+        public void Push(int data)
+        {
+            Node newnode = new Node(data);
+            if (this.top == null)
+            {
+                newnode.next = null;
+            }
+            else
+            {
+                newnode.next = this.top;
+            }
+            this.top = newnode;
+            Console.WriteLine("{0} pushed to stack",data);
+        }
+
+        internal void Display()
+        {
+            Node temp = this.top;
+            if (temp == null)
+            {
+                Console.WriteLine("Empty Linked list");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }     
+    }
+}
+
---
+++ StackDataStruct/StackDataStruct/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StackDataStruct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkListStack linkListstack = new LinkListStack();
+            linkListstack.Push(70);
+            linkListstack.Push(30);
+            linkListstack.Push(56);
+            linkListstack.Display();
+        }
+    }
+}
